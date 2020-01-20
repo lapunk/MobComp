2bcbc146cb15ea3a13b1c6521485c047e0568a78
Laura Punkeri <punkeri.laura@gmail.com>
2020-01-20 18:29:49 +0200

    change implementation

--- app/src/main/java/com/mobcomp/lab/MainActivity.kt
+++ app/src/main/java/com/mobcomp/lab/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.mobcomp.lab
 
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -15,15 +17,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val bt : Button = findViewById (R.id.button_change_color)
-        var color_flag : Boolean = false
+        var colorFlag = false
+        // I have no idea what i am doing
+        val context : Context = applicationContext
 
         bt.setOnClickListener {
-            if ( !color_flag ) {
-                bt.setTextColor(resources.getColor(R.color.winered))
-                color_flag=true
+            if ( !colorFlag ) {
+                bt.setTextColor(ContextCompat.getColor(context, R.color.winered))
+                colorFlag=true
             } else {
-                bt.setTextColor(resources.getColor(R.color.darkgrey))
-                color_flag=false
+                bt.setTextColor(ContextCompat.getColor(context, R.color.darkgrey))
+                colorFlag=false
             }
 
         }
